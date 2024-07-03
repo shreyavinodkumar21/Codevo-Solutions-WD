9660cf802acb6fbb746046947de253a8eb247950
shreyavinodkumar21 <139476155+shreyavinodkumar21@users.noreply.github.com>
2024-07-03 15:37:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const navLinks = document.querySelectorAll('nav a');
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryItems = document.querySelectorAll('.gallery-item img');
 
-navLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const href = this.getAttribute('href');
-    const targetSection = document.querySelector(href);
-    targetSection.scrollIntoView({ behavior: "smooth" });
-  });
+    galleryItems.forEach(item => {
+        item.addEventListener('click', function () {
+            const src = this.getAttribute('src');
+            openLightbox(src);
+        });
+    });
+
+    function openLightbox(src) {
+        const lightbox = document.createElement('div');
+        lightbox.classList.add('lightbox');
+        lightbox.innerHTML = `
+            <div class="lightbox-content">
+                <img src="${src}" alt="Lightbox Image">
+                <span class="lightbox-close">&times;</span>
+            </div>
+        `;
+        document.body.appendChild(lightbox);
+
+        const closeButton = lightbox.querySelector('.lightbox-close');
+        closeButton.addEventListener('click', function () {
+            document.body.removeChild(lightbox);
+        });
+    }
 });
